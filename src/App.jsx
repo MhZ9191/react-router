a64721f9eb0a041ec890b34b3685359497cb8d64
MhZ9191 <hegic91@gmail.com>
2026-02-18 15:59:54 +0100

    Add macro structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index Component={HomePage} />
+        <Route path="/about" Component={AboutPage} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
