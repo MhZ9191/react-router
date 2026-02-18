eead58aa7eb88de1621b14dbd7568f5aca57d941
MhZ9191 <hegic91@gmail.com>
2026-02-18 17:51:02 +0100

    Add minimum css for products

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,15 +1,17 @@
 export default function ProductPage({ products }) {
   return (
     <>
-      <section>
-        <div>
+      <section className="sec-prod">
+        <div className="div-prod">
           {products.map((el) => {
             return (
-              <div key={el.id}>
+              <div key={el.id} className="card-prod">
                 <h2>{el.title}</h2>
-                <figure>
-                  <img src={el.image} alt={el.description} />
-                </figure>
+                <div className="div-image-prod">
+                  <figure>
+                    <img src={el.image} alt={el.description} />
+                  </figure>
+                </div>
                 <p>{el.price + " $"}</p>
               </div>
             );
