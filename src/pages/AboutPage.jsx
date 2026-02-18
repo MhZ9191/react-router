7555d25f721d3374a7eec341cb1a4cc1e6a1f062
MhZ9191 <hegic91@gmail.com>
2026-02-18 16:51:57 +0100

    Add AboutPage css

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,10 +4,33 @@ export default function AboutPage() {
   return (
     <>
       <Header />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
-        expedita?
-      </p>
+      <section className="sec-about">
+        <div className="div-about">
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Cupiditate quas eveniet quo sed provident dolorem vitae laboriosam
+              repellendus fugiat quis. Nostrum voluptatum tenetur, minima
+              corporis dicta deserunt! Sed natus, quasi provident officia, dicta
+              aperiam atque laboriosam vel cupiditate qui quis iusto ipsam
+              delectus veritatis eum? Asperiores dolorem debitis nemo provident.
+            </p>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
+              magnam perspiciatis nihil. Harum ducimus fugiat, ut doloribus
+              reiciendis explicabo illo obcaecati iure molestias, consectetur
+              vitae quia. Velit officiis, ullam facilis molestiae impedit
+              nostrum incidunt! Nesciunt necessitatibus error dolorem explicabo
+              nihil quasi laudantium quidem eaque quis aut similique labore
+              perferendis illum placeat a, ullam laborum, culpa repudiandae, sed
+              eum quia! Odit minus soluta ab vitae! Accusantium mollitia
+              consectetur molestias voluptatem ipsam!
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
