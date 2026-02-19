2ea15378f9d27a4fa9c474cf530b2762121a2051
MhZ9191 <hegic91@gmail.com>
2026-02-19 16:32:17 +0100

    Add link to detail page by id

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,10 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+const baseUrl = "https://fakestoreapi.com/products/";
 
 export default function ProductDetailPage() {
   const { id } = useParams();
-  return (
-    <>
-      <h1>HELLO</h1>
-    </>
-  );
+
+  const [dataCard, setDataCard] = useState();
+
+  const fetchDataCard = () => {
+    axios.get(baseUrl + id).then((res) => {
+      setDataCard(res.data);
+    });
+  };
+
+  useEffect(fetchDataCard, []);
+
+  return dataCard ? <h2>{dataCard.title}</h2> : <h2>Caricamento...</h2>;
 }
